2e7400b119da7f30f4b0a06dc1d6bd31949006f2
OscarMiticanoy <oscarmiticanoy@gmail.com>
2020-06-24 10:06:31 -0500

    cambio de nombre de archivo

---
+++ procedimientoRuleta.sql
@@ -0,0 +1,26 @@
+﻿USE [ruletadb]
+GO
+/****** Object:  StoredProcedure [dbo].[insertRuleta]    Script Date: 24/06/2020 9:44:21 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Oscar Miticanoy>
+-- Create date: <Create Date,,23-06-2020>
+-- Description:	<Description,,insert and return key>
+-- =============================================
+ALTER PROCEDURE [dbo].[insertRuleta]
+	@id int output,
+	@state bit,
+	@number int,
+	@color bit
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO ruletas(state,number,color)
+	VALUES (@state, @number, @color)
+   
+    SELECT SCOPE_IDENTITY();
+END
